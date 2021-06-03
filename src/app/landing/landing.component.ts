64c40659899303905d54ecd744509ca55273cda3
MacWhitehead <mackayla.whitehead7485@stu.mtec.edu>
2021-06-02 19:31:13 -0600

    Adjusted layout, added resume & toggle

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,16 +5,29 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
   faLinkedin = faLinkedin;
   faGithubAlt = faGithubAlt;
   faEnvelope = faEnvelope;
 
-  constructor() { }
+  showSection = false;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleMore() {
+   let toggleBtn = document.getElementById('toggleSkills')!
+    if(this.showSection == false) {
+      this.showSection = true
+      toggleBtn.innerHTML = 'Show Less'
+    }
+    else {
+      this.showSection = false
+      toggleBtn.innerHTML = 'Show More'
+    }
+    console.log('Clicked!')
   }
-
 }
